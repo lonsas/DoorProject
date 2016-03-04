72343ee3cd956d56c5297d5cd12f43f77ea93f95
alexander <lonsas@gmail.com>
2016-03-05 00:21:13 +0100

    Now intended to work with emitter next to the receiver.
    Checks if object in front of led.
    Only turns on esp8266 when sending message.
    Senses if door is opened and closed.

--- doorsensor/doorsensor.cpp
+++ doorsensor/doorsensor.cpp
@@ -1,45 +1,60 @@
 #include "Arduino.h"
+
+#define DEBUG
+
 int sensorPin = A9;
-int ledPin = 13;
+int espEnPin = 3;
+int ledPin = 2;
 int sensorValue = 0;
-int sensorValue2 = 0;
 int thres = 1000;
-bool passed = false;
-bool prevStateOn = false;
-bool play_pressed = false;
+bool doorOpen = false;
+bool prevDoorOpen = false;
+bool in = true;
 void setup() {
   Serial.begin(115200);
   Serial1.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  pinMode(espEnPin, OUTPUT);
+  digitalWrite(espEnPin, HIGH);
 }
 
+void sendCommand() {
+    digitalWrite(espEnPin, HIGH);
+    delay(2000);
+    do {
+        Serial1.write("AT+CIPSTART=\"TCP\",\"31.208.43.115\",8000\r\n");
+        delay(500);
+        Serial1.write("AT+CIPSEND=8\r\n");
+        delay(100);
+        Serial1.write("DOOROPEN");
+        Serial1.write("AT+CIPCLOSE\r\n");
+        delay(500);
+    } while(!Serial1.find("CONNECT"));
+    digitalWrite(espEnPin, LOW);
+}
 void loop() {
-  while(Serial1.available()) {
-    Serial.write(Serial1.read());
-  }
-  sensorValue = analogRead(sensorPin);
-  if(sensorValue < thres) {
-    prevStateOn = true;
-  } else if(prevStateOn) {
-    prevStateOn=false;
-    delay(1000);
-    
-    passed = !passed;
-
-  }
-  if(passed) {
+#ifdef DEBUG
+    while(Serial1.available()) {
+        Serial.write(Serial1.read());
+    }
+#endif
     digitalWrite(ledPin, HIGH);
-    Serial1.write("AT+CIPSTART=\"TCP\",\"31.208.43.115\",8000\r\n");
-    delay(500);
-    Serial1.write("AT+CIPSEND=8\r\n");
-    delay(100);
-    Serial1.write("DOOROPEN");
-    Serial1.write("AT+CIPCLOSE\r\n");
-    passed = false;
-
-  }
-    Serial.println(passed);
+    // Give the led time to shine, doesn't work otherwise
+    delay(5);
+    sensorValue = analogRead(sensorPin);
+    digitalWrite(ledPin, LOW);
+    doorOpen = sensorValue > thres;
+    if(prevDoorOpen && !doorOpen) {
+      // Someone open and closed the door, then they probably passed the door;
+      in = !in;
+      sendCommand();
+    }
+    prevDoorOpen = doorOpen;
+    Serial.println(in);
     Serial.println(thres);
     Serial.println(sensorValue);
     Serial.println();
+    delay(1000);
 }
 
